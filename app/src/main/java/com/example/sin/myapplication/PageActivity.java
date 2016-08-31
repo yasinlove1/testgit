e4a643098c37d9bea43b5cdef9e98ee75bf01e8b
Your Name <sakchaichammalee@gmail.com>
2016-08-31 17:02:51 +0700

    first commit

---
+++ app/src/main/java/com/example/sin/myapplication/PageActivity.java
@@ -0,0 +1,16 @@
+package com.example.sin.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sin on 27/3/2559.
+ */
+public class PageActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_activity);
+
+    }
+}
